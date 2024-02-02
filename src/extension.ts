0ecc7ba98421571b4b7b87b54c7b40503d828b7b
gusalbukrk <gusalbukrk@gmail.com>
2024-02-02 11:45:57 -0300

    translate webview content

--- src/extension.ts
+++ src/extension.ts
@@ -8,10 +8,35 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage(vscode.l10n.t('Hello World from extension!'));
+		// vscode.window.showInformationMessage(vscode.l10n.t('Hello World from extension!'));
+
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding', // Identifies the type of the webview. Used internally
+			'Cat Coding', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
+
+		panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+		<h1>${vscode.l10n.t('Hello World from extension!')}</h1>
+		<h2>${vscode.l10n.t('Your extension got activated with the {0} language!', vscode.env.language)}</h2>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
 
 export function deactivate() {}
