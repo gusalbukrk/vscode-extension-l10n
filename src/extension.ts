27f1fa3102252df0ef7780265447a69de5aaa2c7
gusalbukrk <gusalbukrk@gmail.com>
2024-02-02 10:52:46 -0300

    translate source code strings

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "extension" is now active!');
+	vscode.window.showInformationMessage(
+		// example of string w/ arguments
+		vscode.l10n.t('Your extension got activated with the {0} language!', vscode.env.language)
+	);
 
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from extension!');
+		vscode.window.showInformationMessage(vscode.l10n.t('Hello World from extension!'));
 	});
 
 	context.subscriptions.push(disposable);
